6df3e18f28210fecec782deb86b601bdd50e7214
韩旭 <js_bin@163.com>
2022-07-07 10:11:12 +0800

    feature: mint logic add

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import {
+  mintUsdtByAddressInEth,
+  mintUsdtByAddressInBsc,
+  mintUsdtByAddressInMatic,
+  mintUsdtByAddress
+} from '@utils/usdt'
+
+import {
+  mintUsdcByAddressInEth,
+  mintUsdcByAddressInBsc,
+  mintUsdcByAddressInMatic,
+  mintUsdcByAddress
+} from '@utils/usdc'
+
+import {
+  mintDaiByAddressInEth,
+  mintDaiByAddressInBsc,
+  mintDaiByAddressInMatic,
+  mintDaiByAddress
+} from '@utils/dai'
+
+export {
+  mintUsdtByAddressInEth,
+  mintUsdtByAddressInBsc,
+  mintUsdtByAddressInMatic,
+  mintUsdtByAddress,
+  mintUsdcByAddressInEth,
+  mintUsdcByAddressInBsc,
+  mintUsdcByAddressInMatic,
+  mintUsdcByAddress,
+  mintDaiByAddressInEth,
+  mintDaiByAddressInBsc,
+  mintDaiByAddressInMatic,
+  mintDaiByAddress
+}
---
+++ src/utils/hardhat.js
@@ -0,0 +1,36 @@
+// === Utils === //
+import isArray from 'lodash/isArray';
+import BigNumber from 'bignumber.js';
+
+/**
+ * 身份伪装
+ * @param {*} targetAccounts
+ * @returns
+ */
+export const impersonates = async (targetAccounts) => {
+  if (!isArray(targetAccounts)) return new Error('must be a array');
+  await hre.network.provider.request({
+    method: 'hardhat_impersonateAccount',
+    params: targetAccounts,
+  });
+  return async () => {
+    await hre.network.provider.request({
+      method: 'hardhat_stopImpersonatingAccount',
+      params: targetAccounts,
+    });
+  };
+};
+
+export const sendEthers = (
+  reviver,
+  amount = new BigNumber(10).pow(18).multipliedBy(10),
+) => {
+  if (!BigNumber.isBigNumber(amount)) {
+    return new Error('must be a bignumber.js object');
+  }
+
+  return hre.network.provider.send('hardhat_setBalance', [
+    reviver,
+    `0x${amount.toString(16)}`,
+  ]);
+};
