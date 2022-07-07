5175508fb5ec653b3c55845810caeb33d4552455
韩旭 <js_bin@163.com>
2022-07-07 10:10:23 +0800

    chore: add interface

---
+++ src/interface/IERC20_BSC.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+import "./IERC20.sol";
+
+interface IERC20_BSC is IERC20 {
+    function mint(uint256 amount) external;
+
+    function getOwner() external view returns (address);
+}
---
+++ src/interface/IERC20_ETH.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+import "./IERC20.sol";
+
+interface IERC20_ETH is IERC20 {
+    function mint(address _to, uint256 amount) external;
+
+    function isMinter(address account) external view returns (bool);
+
+    function masterMinter() external view returns (address);
+
+    function configureMinter(address minter, uint256 minterAllowedAmount)
+        external
+        returns (bool);
+
+    function issue(uint256 amount) external;
+
+    function owner() external view returns (address);
+
+    function deposit(address user, bytes calldata depositData) external;
+}
