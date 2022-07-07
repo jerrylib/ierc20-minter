accf69360b0a3ca5be0090e65b3af12b4cfe0f33
韩旭 <js_bin@163.com>
2022-07-07 10:10:04 +0800

    chore: add hardhat

---
+++ hardhat-eth.config.js
@@ -0,0 +1,21 @@
+const config = require('./hardhat.config')
+
+const networks = {
+  hardhat: {
+    forking: {
+      url:
+        'https://eth-mainnet.alchemyapi.io/v2/RbuvkoqtcsoGKG0__pVEKQWjh8cLNyV1',
+      blockNumber: 14954555 // <-- edit here
+    },
+    timeout: 1800000
+  },
+  localhost: {
+    url: 'http://localhost:8545',
+    timeout: 1800000
+  }
+}
+
+module.exports = {
+  ...config,
+  networks
+}
---
+++ hardhat-matic.config.js
@@ -0,0 +1,21 @@
+const config = require('./hardhat.config')
+
+const networks = {
+  hardhat: {
+    forking: {
+      url:
+        'https://polygon-mainnet.g.alchemy.com/v2/Adoz7Rt1NnymP8CLPdhkTLHKncqQXkKz',
+      blockNumber: 28787261 // <-- edit here
+    },
+    timeout: 1800000
+  },
+  localhost: {
+    url: 'http://localhost:8545',
+    timeout: 1800000
+  }
+}
+
+module.exports = {
+  ...config,
+  networks
+}
