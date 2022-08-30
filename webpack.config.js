f72d414073093fa98f0dbc97d63f7dbab21e6acc
韩旭 <js_bin@163.com>
2022-08-30 20:14:44 +0800

    chore: alias update

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,42 @@
-const path = require("path");
-const TerserPlugin = require("terser-webpack-plugin");
+const path = require('path')
+const TerserPlugin = require('terser-webpack-plugin')
 
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   entry: {
-    "ierc20-minter": path.join(__dirname, "src/index.js"),
-    "ierc20-minter.min": path.join(__dirname, "src/index.js"),
+    'ierc20-minter': path.join(__dirname, 'src/index.js'),
+    'ierc20-minter.min': path.join(__dirname, 'src/index.js')
   },
   output: {
-    path: path.join(__dirname, "dist"),
-    filename: "[name].js",
-    library: "minter",
-    libraryExport: "default",
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    library: 'minter',
+    libraryExport: 'default'
   },
-  mode: "none",
+  mode: 'none',
   module: {
     rules: [
       {
-        loader: "babel-loader",
+        loader: 'babel-loader',
         test: /\.js?$/,
-        exclude: /(node_modules)/,
-      },
-    ],
+        exclude: /(node_modules)/
+      }
+    ]
   },
   resolve: {
     alias: {
-      "@constants": path.join(__dirname, "src/constants"),
-      "@utils": path.join(__dirname, "src/utils"),
+      '@': path.join(__dirname, 'src')
     },
-    extensions: ["", ".js"],
+    extensions: ['', '.js']
   },
   plugins: [new CleanWebpackPlugin()],
   optimization: {
     minimize: true,
     minimizer: [
       new TerserPlugin({
-        include: /\.min\.js$/,
-      }),
-    ],
-  },
-};
+        include: /\.min\.js$/
+      })
+    ]
+  }
+}
