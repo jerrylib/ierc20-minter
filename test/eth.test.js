70d9a876a1968c55d0148f0afd8bf0632811ad31
韩旭 <js_bin@163.com>
2022-08-30 20:20:15 +0800

    tests: testcases update

--- test/eth.test.js
+++ test/eth.test.js
@@ -2,60 +2,158 @@ import {
   mintUsdtByAddressInEth,
   mintUsdcByAddressInEth,
   mintDaiByAddressInEth,
-} from "../src/index";
-import { ethers } from "hardhat";
+  mintEthByAddress,
+  mintGusdByAddressInEth,
+  mintLusdByAddressInEth,
+  mintREth2ByAddressInEth,
+  mintRocketPoolEthByAddressInEth,
+  mintSEthByAddressInEth,
+  mintSEth2ByAddressInEth,
+  mintStEthByAddressInEth,
+  mintSusdByAddressInEth,
+  mintTusdByAddressInEth,
+  mintUsdpByAddressInEth,
+  mintWethByAddressInEth,
+  mintWstEthByAddressInEth
+} from '../src'
+import { ethers } from 'hardhat'
 
 // === Utils === //
-import assert from "assert";
-import BigNumber from "bignumber.js";
+import assert from 'assert'
+import BigNumber from 'bignumber.js'
 
 // === Constants === //
-import { USDT_ETH, USDC_ETH, DAI_ETH } from "@constants/Address";
+import { USDT_ETH, USDC_ETH, DAI_ETH, LUSD_ETH, GUSD_ETH } from '@/constants/Address'
 
-const IERC20 = hre.artifacts.require("@interface/IERC20.sol");
+const IERC20 = hre.artifacts.require('@interface/IERC20.sol')
 
-let accounts;
-let farmer1;
-describe("minter test for ETH", function () {
+let accounts
+let farmer1
+describe('minter test for ETH', function () {
   before(async function () {
-    await ethers.getSigners().then((resp) => {
-      accounts = resp;
-      farmer1 = accounts[19].address;
-    });
-  });
+    await ethers.getSigners().then(resp => {
+      accounts = resp
+      farmer1 = accounts[19].address
+    })
+  })
 
-  it("mint Usdt in eth", async function () {
-    const contract = await IERC20.at(USDT_ETH);
-    const topUpAmount = new BigNumber(10).pow(6 + 9);
-    const balanceBefore = await contract.balanceOf(farmer1);
-    await mintUsdtByAddressInEth(farmer1, topUpAmount);
-    assert.equal(
-      (await contract.balanceOf(farmer1)).sub(balanceBefore).toString(),
-      topUpAmount.toString()
-    );
-  });
+  it('mint Usdt in eth', async function () {
+    const contract = await IERC20.at(USDT_ETH)
+    const topUpAmount = new BigNumber(10).pow(6 + 9)
+    const balanceBefore = await contract.balanceOf(farmer1)
+    await mintUsdtByAddressInEth(farmer1, topUpAmount)
+    assert.equal((await contract.balanceOf(farmer1)).sub(balanceBefore).toString(), topUpAmount.toString())
+  })
 
-  it("mint Usdc in eth", async function () {
-    const contract = await IERC20.at(USDC_ETH);
-    const topUpAmount = new BigNumber(10).pow(6 + 9);
-    const balanceBefore = await contract.balanceOf(farmer1);
-    await mintUsdcByAddressInEth(farmer1, topUpAmount);
-    assert.equal(
-      (await contract.balanceOf(farmer1)).sub(balanceBefore).toString(),
-      topUpAmount.toString()
-    );
-  });
+  it('mint Usdc in eth', async function () {
+    const contract = await IERC20.at(USDC_ETH)
+    const topUpAmount = new BigNumber(10).pow(6 + 9)
+    const balanceBefore = await contract.balanceOf(farmer1)
+    await mintUsdcByAddressInEth(farmer1, topUpAmount)
+    assert.equal((await contract.balanceOf(farmer1)).sub(balanceBefore).toString(), topUpAmount.toString())
+  })
 
-  it("mint Dai in eth", async function () {
-    const contract = await IERC20.at(DAI_ETH);
-    const topUpAmount = new BigNumber(10).pow(18 + 9);
-    const balanceBefore = await contract.balanceOf(farmer1);
-    await mintDaiByAddressInEth(farmer1, topUpAmount);
-    assert.equal(
-      new BigNumber(
-        (await contract.balanceOf(farmer1)).sub(balanceBefore)
-      ).toString(),
-      topUpAmount.toString()
-    );
-  });
-});
+  it('mint Dai in eth', async function () {
+    const contract = await IERC20.at(DAI_ETH)
+    const topUpAmount = new BigNumber(10).pow(18 + 9)
+    const balanceBefore = await contract.balanceOf(farmer1)
+    await mintDaiByAddressInEth(farmer1, topUpAmount)
+    assert.equal(new BigNumber((await contract.balanceOf(farmer1)).sub(balanceBefore)).toString(), topUpAmount.toString())
+  })
+
+  it('mint Lusd in eth', async function () {
+    const contract = await IERC20.at(LUSD_ETH)
+    const topUpAmount = new BigNumber(10).pow(18 + 9)
+    const balanceBefore = await contract.balanceOf(farmer1)
+    await mintLusdByAddressInEth(farmer1, topUpAmount)
+    assert.equal(new BigNumber((await contract.balanceOf(farmer1)).sub(balanceBefore)).toString(), topUpAmount.toString())
+  })
+
+  it('mint Gusd in eth', async function () {
+    const contract = await IERC20.at(GUSD_ETH)
+    const topUpAmount = new BigNumber(10).pow(18 + 9)
+    const balanceBefore = await contract.balanceOf(farmer1)
+    await mintGusdByAddressInEth(farmer1, topUpAmount)
+    assert.equal(new BigNumber((await contract.balanceOf(farmer1)).sub(balanceBefore)).toString(), topUpAmount.toString())
+  })
+
+  it('mint rEth2 in eth', async function () {
+    const contract = await IERC20.at(RETH2_ETH)
+    const topUpAmount = new BigNumber(10).pow(18 + 9)
+    const balanceBefore = await contract.balanceOf(farmer1)
+    await mintREth2ByAddressInEth(farmer1, topUpAmount)
+    assert.equal(new BigNumber((await contract.balanceOf(farmer1)).sub(balanceBefore)).toString(), topUpAmount.toString())
+  })
+
+  it('mint rocketPoolEth in eth', async function () {
+    const contract = await IERC20.at(ROCKET_POOL_ETH_ETH)
+    const topUpAmount = new BigNumber(10).pow(18 + 9)
+    const balanceBefore = await contract.balanceOf(farmer1)
+    await mintRocketPoolEthByAddressInEth(farmer1, topUpAmount)
+    assert.equal(new BigNumber((await contract.balanceOf(farmer1)).sub(balanceBefore)).toString(), topUpAmount.toString())
+  })
+
+  it('mint sEth in eth', async function () {
+    const contract = await IERC20.at(SETH_ETH)
+    const topUpAmount = new BigNumber(10).pow(18 + 9)
+    const balanceBefore = await contract.balanceOf(farmer1)
+    await mintSEthByAddressInEth(farmer1, topUpAmount)
+    assert.equal(new BigNumber((await contract.balanceOf(farmer1)).sub(balanceBefore)).toString(), topUpAmount.toString())
+  })
+
+  it('mint sEth2 in eth', async function () {
+    const contract = await IERC20.at(SETH2_ETH)
+    const topUpAmount = new BigNumber(10).pow(18 + 9)
+    const balanceBefore = await contract.balanceOf(farmer1)
+    await mintSEth2ByAddressInEth(farmer1, topUpAmount)
+    assert.equal(new BigNumber((await contract.balanceOf(farmer1)).sub(balanceBefore)).toString(), topUpAmount.toString())
+  })
+
+  it('mint stEth in eth', async function () {
+    const contract = await IERC20.at(STETH_ETH)
+    const topUpAmount = new BigNumber(10).pow(18 + 9)
+    const balanceBefore = await contract.balanceOf(farmer1)
+    await mintStEthByAddressInEth(farmer1, topUpAmount)
+    assert.equal(new BigNumber((await contract.balanceOf(farmer1)).sub(balanceBefore)).toString(), topUpAmount.toString())
+  })
+
+  it('mint sUsd in eth', async function () {
+    const contract = await IERC20.at(SUSD_ETH)
+    const topUpAmount = new BigNumber(10).pow(18 + 9)
+    const balanceBefore = await contract.balanceOf(farmer1)
+    await mintSusdByAddressInEth(farmer1, topUpAmount)
+    assert.equal(new BigNumber((await contract.balanceOf(farmer1)).sub(balanceBefore)).toString(), topUpAmount.toString())
+  })
+
+  it('mint tUsd in eth', async function () {
+    const contract = await IERC20.at(TUSD_ETH)
+    const topUpAmount = new BigNumber(10).pow(18 + 9)
+    const balanceBefore = await contract.balanceOf(farmer1)
+    await mintTusdByAddressInEth(farmer1, topUpAmount)
+    assert.equal(new BigNumber((await contract.balanceOf(farmer1)).sub(balanceBefore)).toString(), topUpAmount.toString())
+  })
+
+  it('mint usdp in eth', async function () {
+    const contract = await IERC20.at(USDP_ETH)
+    const topUpAmount = new BigNumber(10).pow(18 + 9)
+    const balanceBefore = await contract.balanceOf(farmer1)
+    await mintUsdpByAddressInEth(farmer1, topUpAmount)
+    assert.equal(new BigNumber((await contract.balanceOf(farmer1)).sub(balanceBefore)).toString(), topUpAmount.toString())
+  })
+
+  it('mint weth in eth', async function () {
+    const contract = await IERC20.at(WETH_ETH)
+    const topUpAmount = new BigNumber(10).pow(18 + 9)
+    const balanceBefore = await contract.balanceOf(farmer1)
+    await mintWethByAddressInEth(farmer1, topUpAmount)
+    assert.equal(new BigNumber((await contract.balanceOf(farmer1)).sub(balanceBefore)).toString(), topUpAmount.toString())
+  })
+
+  it('mint wstEth in eth', async function () {
+    const contract = await IERC20.at(WSTETH_ETH)
+    const topUpAmount = new BigNumber(10).pow(18 + 9)
+    const balanceBefore = await contract.balanceOf(farmer1)
+    await mintWstEthByAddressInEth(farmer1, topUpAmount)
+    assert.equal(new BigNumber((await contract.balanceOf(farmer1)).sub(balanceBefore)).toString(), topUpAmount.toString())
+  })
+})
--- test/matic.test.js
+++ test/matic.test.js
@@ -1,65 +1,46 @@
-import {
-  mintUsdtByAddressInMatic,
-  mintUsdcByAddressInMatic,
-  mintDaiByAddressInMatic,
-} from "../src/index";
-import { ethers } from "hardhat";
+import { mintUsdtByAddressInMatic, mintUsdcByAddressInMatic, mintDaiByAddressInMatic } from '../src'
+import { ethers } from 'hardhat'
 
 // === Utils === //
-import assert from "assert";
-import BigNumber from "bignumber.js";
+import assert from 'assert'
+import BigNumber from 'bignumber.js'
 
 // === Constants === //
-import { USDT_MATIC, USDC_MATIC, DAI_MATIC } from "@constants/Address";
+import { USDT_MATIC, USDC_MATIC, DAI_MATIC } from '@/constants/Address'
 
-const IERC20 = hre.artifacts.require("@interface/IERC20.sol");
+const IERC20 = hre.artifacts.require('@interface/IERC20.sol')
 
-let accounts;
-let farmer1;
-describe("minter test for MATIC", function () {
+let accounts
+let farmer1
+describe('minter test for MATIC', function () {
   before(async function () {
-    await ethers.getSigners().then((resp) => {
-      accounts = resp;
-      farmer1 = accounts[19].address;
-    });
-  });
+    await ethers.getSigners().then(resp => {
+      accounts = resp
+      farmer1 = accounts[19].address
+    })
+  })
 
-  it("mint Usdt in MATIC", async function () {
-    const contract = await IERC20.at(USDT_MATIC);
-    const topUpAmount = new BigNumber(10).pow(18 + 9);
-    const balanceBefore = await contract.balanceOf(farmer1);
-    await mintUsdtByAddressInMatic(farmer1, topUpAmount);
-    assert.equal(
-      new BigNumber(
-        (await contract.balanceOf(farmer1)).sub(balanceBefore)
-      ).toString(),
-      topUpAmount.toString()
-    );
-  });
+  it('mint Usdt in MATIC', async function () {
+    const contract = await IERC20.at(USDT_MATIC)
+    const topUpAmount = new BigNumber(10).pow(18 + 9)
+    const balanceBefore = await contract.balanceOf(farmer1)
+    await mintUsdtByAddressInMatic(farmer1, topUpAmount)
+    assert.equal(new BigNumber((await contract.balanceOf(farmer1)).sub(balanceBefore)).toString(), topUpAmount.toString())
+  })
 
-  it("mint Usdc in MATIC", async function () {
-    const contract = await IERC20.at(USDC_MATIC);
-    const topUpAmount = new BigNumber(10).pow(18 + 9);
-    const balanceBefore = await contract.balanceOf(farmer1);
-    await mintUsdcByAddressInMatic(farmer1, topUpAmount);
-    assert.equal(
-      new BigNumber(
-        (await contract.balanceOf(farmer1)).sub(balanceBefore)
-      ).toString(),
-      topUpAmount.toString()
-    );
-  });
+  it('mint Usdc in MATIC', async function () {
+    const contract = await IERC20.at(USDC_MATIC)
+    const topUpAmount = new BigNumber(10).pow(18 + 9)
+    const balanceBefore = await contract.balanceOf(farmer1)
+    await mintUsdcByAddressInMatic(farmer1, topUpAmount)
+    assert.equal(new BigNumber((await contract.balanceOf(farmer1)).sub(balanceBefore)).toString(), topUpAmount.toString())
+  })
 
-  it("mint Dai in MATIC", async function () {
-    const contract = await IERC20.at(DAI_MATIC);
-    const topUpAmount = new BigNumber(10).pow(18 + 9);
-    const balanceBefore = await contract.balanceOf(farmer1);
-    await mintDaiByAddressInMatic(farmer1, topUpAmount);
-    assert.equal(
-      new BigNumber(
-        (await contract.balanceOf(farmer1)).sub(balanceBefore)
-      ).toString(),
-      topUpAmount.toString()
-    );
-  });
-});
+  it('mint Dai in MATIC', async function () {
+    const contract = await IERC20.at(DAI_MATIC)
+    const topUpAmount = new BigNumber(10).pow(18 + 9)
+    const balanceBefore = await contract.balanceOf(farmer1)
+    await mintDaiByAddressInMatic(farmer1, topUpAmount)
+    assert.equal(new BigNumber((await contract.balanceOf(farmer1)).sub(balanceBefore)).toString(), topUpAmount.toString())
+  })
+})
