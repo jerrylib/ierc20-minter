a76ee87747f426a23daf3bc7901af3387618289d
韩旭 <js_bin@163.com>
2022-08-30 20:18:19 +0800

    chore: hardhat config update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,82 +1,82 @@
-require("@babel/register");
-require("@nomiclabs/hardhat-truffle5");
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-ethers");
+require('@babel/register')
+require('@nomiclabs/hardhat-truffle5')
+require('@nomiclabs/hardhat-waffle')
+require('@nomiclabs/hardhat-ethers')
 
-process.env.FORCE_COLOR = "3";
-process.env.TS_NODE_TRANSPILE_ONLY = "true";
+process.env.FORCE_COLOR = '3'
+process.env.TS_NODE_TRANSPILE_ONLY = 'true'
 
-const DEFAULT_BLOCK_GAS_LIMIT = 30000000;
+const DEFAULT_BLOCK_GAS_LIMIT = 30000000
 
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
+task('accounts', 'Prints the list of accounts', async () => {
+  const accounts = await ethers.getSigners()
 
   for (const account of accounts) {
-    console.log(account.address);
+    console.log(account.address)
   }
-});
+})
 
 const config = {
-  defaultNetwork: "hardhat",
+  defaultNetwork: 'hardhat',
   networks: {
     hardhat: {
       forking: {
-        url: "https://eth-mainnet.alchemyapi.io/v2/RbuvkoqtcsoGKG0__pVEKQWjh8cLNyV1",
-        blockNumber: 14954555, // <-- edit here
+        url: 'https://eth-mainnet.alchemyapi.io/v2/RbuvkoqtcsoGKG0__pVEKQWjh8cLNyV1',
+        blockNumber: 14954555 // <-- edit here
       },
       blockGasLimit: DEFAULT_BLOCK_GAS_LIMIT,
       timeout: 1800000,
-      allowUnlimitedContractSize: true,
+      allowUnlimitedContractSize: true
     },
     localhost: {
-      url: "http://localhost:8545",
+      url: 'http://localhost:8545',
       allowUnlimitedContractSize: true,
-      timeout: 1800000,
-    },
+      timeout: 1800000
+    }
   },
   solidity: {
     compilers: [
       {
-        version: "0.6.12",
+        version: '0.6.12',
         settings: {
           optimizer: {
             details: {
-              yul: false,
+              yul: false
             },
             enabled: true,
-            runs: 200,
-          },
-        },
+            runs: 200
+          }
+        }
       },
       {
-        version: "0.8.3",
+        version: '0.8.3',
         settings: {
           optimizer: {
             details: {
-              yul: true,
+              yul: true
             },
             enabled: true,
-            runs: 200,
-          },
-        },
-      },
-    ],
+            runs: 200
+          }
+        }
+      }
+    ]
   },
   paths: {
-    sources: "./src/interface",
+    sources: './src/interface'
   },
   mocha: {
-    timeout: 2000000,
+    timeout: 2000000
   },
   gasReporter: {
     enabled: true,
-    currency: "USD",
+    currency: 'USD'
   },
   contractSizer: {
     alphaSort: true,
     runOnCompile: true,
-    disambiguatePaths: false,
-  },
-};
+    disambiguatePaths: false
+  }
+}
 
-module.exports = config;
+module.exports = config
